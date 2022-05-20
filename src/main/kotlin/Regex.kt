afc723a6d26372ac2b3da5950cf294c501f2fe41
madsunrise <rudnev.vanya@gmail.com>
2022-05-20 17:47:23 +0300

    Fixed bug in regex again

--- src/main/kotlin/Regex.kt
+++ src/main/kotlin/Regex.kt
@@ -3,6 +3,6 @@ package main.kotlin
 import java.util.regex.Pattern
 
 object Regex {
-    val BEGIN_REGEX = Pattern.compile("<string name=\"[a-z|_]+\">")
+    val BEGIN_REGEX = Pattern.compile("<string name=\"[a-zA-Z0-9_]+\">")
     const val LINE_END = "</string>"
 }
